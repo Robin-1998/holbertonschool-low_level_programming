c082cf18aeec5e389977d386dcb6291953bcc248
Robin-1998 <10616@holbertonstudents.com>
2025-02-17 10:57:55 +0000

    Correctif slide 4 1.2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Reurn: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char texte[] = "Programming is like building a multilingual puzzle";
-	puts (texte);
+	char texte[] = "\"Programming is like building a multilingual puzzle";
+	puts(texte);
 	return(0);
 }
