5a60664aec7bf76c977abcf6e94bbaebbe8565eb
Robin-1998 <10616@holbertonstudents.com>
2025-02-21 10:07:02 +0000

    Slide 7, adding a new file

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n : value of the last digit
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	n %= 10;
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	_putchar('0' + n);
+	return (n);
+}
