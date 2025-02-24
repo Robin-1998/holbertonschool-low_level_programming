84a97441b544e26f3dc3103b9c05f477ada5c7f2
Robin-1998 <10616@holbertonstudents.com>
2025-02-24 10:43:24 +0000

    Projet C - More functions, more nested loops - slide 4, adding a new file

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print the number, from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		if (i != 2 &&  i != 4)
+	{
+		_putchar((i % 10) + '0');
+	}
+	_putchar('\n');
+}
