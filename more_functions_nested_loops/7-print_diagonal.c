38fd38894145373d08171ddbfc4f78c47e8ae637
Robin-1998 <10616@holbertonstudents.com>
2025-02-25 08:40:06 +0000

    Correctif slide 7 1.2

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,24 @@
 
 void print_diagonal(int n)
 {
-	if (n > 0)
-	{
-		int i;
+if (n > 0)
+{
+int i;
 
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
+for (i = 0; i < n; i++)
+{
+int j;
+
+for (j = 0; j < n; j++)
+{
+if (j == i)
+_putchar(92);
+else
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
 	else
 	{
 		_putchar ('\n');
