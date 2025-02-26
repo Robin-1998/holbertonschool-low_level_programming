378d7887570f9e8ba536fb03cb680795a86550a1
Robin-1998 <10616@holbertonstudents.com>
2025-02-26 15:05:39 +0000

    Slide 5, adding a new file

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string  - function that prints a string, followed by a new line
+ * @s : reverse
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int *j= &i;
+
+	for (i = 0; i[s] != '\0'; i++);
+	if (i[s] > 9)
+	{
+		for (i = 0; i);
+	}
+}
