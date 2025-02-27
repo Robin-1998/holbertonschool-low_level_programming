1c767c5c95d054bfa53c5282446a975818f7fa14
Robin-1998 <10616@holbertonstudents.com>
2025-02-27 14:45:53 +0000

    Correctif slide 5, projet c pointeur

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "2-strlen.c"
 /**
  * rev_string  - function that prints a string, followed by a new line
  * @s : reverse
@@ -8,12 +8,33 @@
 
 void rev_string(char *s)
 {
-	int i;
-	int *j= &i;
+int i;
+char temp;
+int longueur;
 
-	for (i = 0; i[s] != '\0'; i++);
-	if (i[s] > 9)
-	{
-		for (i = 0; i);
-	}
+longueur = _strlen(s);
+
+for (i = 0; i < longueur / 2; i++)
+{
+temp = s[i];
+/*
+ * On sauvegarde notre premier caractere
+ * de la chaine dans la variable temporaire temp
+ */
+s[i] = s[longueur - i  - 1];
+/*
+ * on echange notre caractere de
+ * la chaine depuis les deux
+ * extremites vers le centre
+ */
+s[longueur - i - 1] = temp;
+}
+/*
+ * on remplace le caractere
+ * a la position symetrique par le
+ * caractere que nous avons stock
+ * dans temp au debut de literation
+ * ce qui finalise l echange
+ * Merci Gwendal
+ */
 }
