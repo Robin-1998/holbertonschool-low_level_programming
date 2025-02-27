84d1a5bead26253ffc697b02c092f1bb41666722
Robin-1998 <10616@holbertonstudents.com>
2025-02-27 15:56:55 +0000

    Projet C pointeur, slide 8, add a new file

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a : pointers
+ * @n : init
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++) /* boucle tout les elements du tableau */
+	{
+		if (i > 0)
+		{
+			printf(", ");
+		}
+		printf("%d", *(a +i));
+	}
+	printf("\n");
+}
