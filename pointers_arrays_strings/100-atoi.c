a0d7a60d0312564a628368fba3f54d536868e1c5
Robin-1998 <10616@holbertonstudents.com>
2025-02-28 11:54:41 +0000

    Projet C, pointeur, slide 10, correctif

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,17 @@ int _atoi(char *s)
 	int i = 0;
 	int result = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= 0 && s[i] <= 9)
-		{
+	    while (s[i] != '\0')
+	    {
+		    if (s[i] >= '0' && s[i] <= '9')
+		    {
 			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return s[i];
+		    }
+		    else
+		    {
+			    return (0);
+		    }
+		    i++;
+	    }
+	    return (result);
 }
