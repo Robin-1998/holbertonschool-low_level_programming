090036679d5966f029f4617c57eaeb060d36e2a9
Robin-1998 <10616@holbertonstudents.com>
2025-02-28 08:39:10 +0000

    Projet c pointeur, slide 9, add a new file

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,30 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s : length
+ * _strcpy - copie la chaine de caractere pointer par src
+ *  including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest : valeur retourner qui est copier du pointeur tableau src
+ * @src : est notre pointeur contenant notre premier tableau boucler
+ * qui est ensuite copier sur dest
  * Return: Always 0.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int copy = 0;
-	return (*dest);
+	int i;
+
+	for (i = 0; i[src] != '\0'; i++)
+/*
+ * initialise la boucle du pointeur tableau src qui commence
+ * du premier caractere au dernier caractere
+*/
+	{
+		i[dest] = i[src];
+/*
+ * Les valeurs du pointeurs tableau de src sont copier dans les valeur du
+ * pointeur tableau dest
+*/
+	}
+	return (dest);
+/* On retourne la valeur de dest qui sera la meme que src */
 }
