4174ebc24e53660b27ca91eedef72f32ee0bd03e
Robin-1998 <10616@holbertonstudents.com>
2025-03-01 18:53:32 +0100

    Correctif slide 1 1.1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -16,12 +16,22 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 	;
+	/* la chaîne dest est parcouru jusqu'à trouver atteindre la fin
+	* de la fin de la chaîne de caractère
+	*/
 	for (j = 0; j < n && src[j] != '\0';)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+	/* la boucle permet de copier des caractères de src vers dest
+	* a chaque itération de la boucle, un caractère de src est copié dans dest
+	* après chaque copie, on passe à la position suivante dans dest (grâce à i++)
+	* si src est plus court que n, on copie tous les caractères de src jusqu'au
+	* dernier caractère
+	*/
 	dest[i] = '\0';
+	/* on s'assure que dest[i] s'arrête au dernier caractere*/
 	return (dest);
 }
