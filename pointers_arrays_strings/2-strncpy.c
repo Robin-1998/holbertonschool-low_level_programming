fcd1f770dd4b105b56a0a6637b4feb1944fcedd2
Robin-1998 <10616@holbertonstudents.com>
2025-03-02 09:37:12 +0100

    projet c pointeur, slide 2, add a new file

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a : first value
+ * @b : second value
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+    for ( ; i < n; i++)
+        dest[i] = '\0';
+    return dest;
+}
