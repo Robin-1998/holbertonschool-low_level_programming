ee72b39229b4fd725603271efb4ad0aad4196f3f
Robin-1998 <10616@holbertonstudents.com>
2025-03-03 11:46:38 +0100

    Projet C, pointeur, add a new file for task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,15 +1,23 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
- * rev_string  - function that prints a string, followed by a new line
- * @s : reverse
+ * _memset - fonction qui remplit la mémoire (s) avec une valeur constante
+ * @s : pointeur qui récupère les caractère (valeur) de b
+ * @b : variable de caractère qui sont uilisés pour les copier dans le
+ * pointeur de tableau (s)
+ * @n : est une varaible qui ne peut être que positive qui sert à indiquer
+ * par exemple dans ce code, tant que notre variable i est inférieur à notre
+ * variable n on incrémente
  * Return: Always 0.
  */
 
- char *_memset(char *s, char b, unsigned int n)
- {
-	int i = 0;
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
 
-	for (i = 0; i < b; i++)
+	for (i = 0; i < n ; i++)
+	{
+	s[i] = b;
+	}
 	return (s);
- }
+}
