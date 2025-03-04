056b5556ea7841c0c825cf3bbbebd37382d7bb3e
Robin-1998 <10616@holbertonstudents.com>
2025-03-04 11:13:51 +0100

    Ajout du fichier pour la task 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -6,17 +6,28 @@
  * Return: Always 0.
  */
 
- char *_strpbrk(char *s, char *accept)
- {
+char *_strpbrk(char *s, char *accept)
+{
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; s[i] != '\0'; i++);
-	for (j = 0; accept[j] != '\0'; i++);
-	if (s[i] == accept[j])
-		return (s + i && accept + j);
-	else
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	return (0);
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			return (&s[i]);
 		}
- }
+	}
+	return (0);
+}
+
+ /*
+ * tant que les caractères du pointeur tableau s ne sont pas arrivés à la fin
+ * on incrémente et dans cette boucle ensuite on imbrique une boucle dans la
+ * première avec le pointeur j qui identifiera la seconde chaîne ("world").
+ * Et si le pointeur du tableau (s) est égal au deuxième pointeur du
+ * tableau (accept) on retourn l'adresse du tableau du premier pointeur
+ * si ces conditions ne sont pas remplis on affiche rien.
+ */
+
