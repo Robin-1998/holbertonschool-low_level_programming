09304eb0e92b76355ec2aab36a03f735d9454bd9
Robin-1998 <10616@holbertonstudents.com>
2025-03-05 22:01:42 +0100

    Projet c, recursion, fichier a rectifié task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function qui retourne la racine carrée d'un nombre
+ * @n :
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n * n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1);
+	}
+}
