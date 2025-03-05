5c0a102699cc841cc78d9432287fc04ed6aef9d0
Robin-1998 <10616@holbertonstudents.com>
2025-03-05 14:39:35 +0100

    Projet C, récursion. Ajout d'un nouveau fichier pour la task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursionn - affiche une chaine de caractère en inverse suivi
+ * d'une nouvelle ligne
+ * @s : pointeur de la chaine de caractère
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
+
+/*
+ * Dans ce code on récursive en utilisant une condition que si notre pointeur
+à bien atteint la fin de la chaine donc on retourne simplement.
+Le fait d'appeler _print_rev_recursion nous permet d'avancer dans la chaîne
+de caractères (en passant aui suivant avec + 1). ça permet d'atteindre la fin
+de la chaine avant de commencer à afficher les caractères en inverse.
+*/
