8314a20a709eb6ef3ffe9cac8170ed45abec28c5
Robin-1998 <10616@holbertonstudents.com>
2025-03-06 16:56:20 +0100

    Correctif slide 1 for project argc, argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,14 +4,14 @@
  * main - function qui affiche le nombre d'argument suivant le nom du
  * fichier transmis
  * @argc : Contiens le nombre d'argument
- * @argv : Contiens le tableau (caractère) des arguments de ligne de commande
- * du programme
+ * @argv : ai censé contenir le nombre de caractère mais vu qu'on n'utilise
+ * pas cette variable, on fait un (void)
  * Return: Always 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
-	return 0;
+	return (0);
 }
