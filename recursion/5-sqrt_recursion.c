ea219ed68a345292840f8d0ddb251ae49a0b5267
Robin-1998 <10616@holbertonstudents.com>
2025-03-06 09:26:00 +0100

    Slide 5, correctif projet recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,17 +2,46 @@
 
 /**
  * _sqrt_recursion - function qui retourne la racine carrée d'un nombre
- * @n :
+ * @n : varible de n qui stocke la v
  * Return: Always 0.
  */
-int _sqrt_recursion(int n)
+
+ int _sqrt_recursion(int n)
 {
-	if (n * n == 0)
-	{
-		return (n);
-	}
-	else
+	if (n < 0)
 	{
 		return (-1);
 	}
+	else
+	{
+		return (recurs2(n, 1));
+	}
 }
+/* Si notre valeur est un chiffre négatif on retourne -1 */
+
+int recurs2(int na, int carre)
+{
+	if (carre * carre == na)
+	{
+		return (carre);
+	}
+	if (carre * carre > na)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (recurs2(na, carre + 1));
+	}
+}
+/* si notre varible carre est na alors on retourn notre racine carré*/
+/* qui est trouvé*/
+
+/* si notre variable n'est pas égal à la racine carré alors on retourne*/
+/* une erreur (return -1)*/
+
+/*
+* ensuite sinon on test les différentes valeurs successives variable de
+carre pour voir si la racine carré est égal à la variable na ou si elle
+ne l'est pas
+*/
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function qui renvoie 1 si l'entier est un nombre entier
+ * sinon 0
+ * @n : variable de l'entier
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+	if (i % i > n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
