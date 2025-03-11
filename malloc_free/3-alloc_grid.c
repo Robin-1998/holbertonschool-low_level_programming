e2d45b95297f2166ab5467bf772a619ad10c1b96
Robin-1998 <10616@holbertonstudents.com>
2025-03-11 14:29:39 +0100

    Correctif task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 {
 	/* Partie 1 */
 	int **tableau;
-	int i;
+	int i, j;
 
 	/* Partie 2 */
 	if (width <= 0)
@@ -27,28 +27,45 @@ int **alloc_grid(int width, int height)
 	}
 
 	/* Partie 3*/
+	tableau = (int**)malloc(sizeof(int*) * height);
+	/*if (height == 0) */
+	/* return (NULL); */
 
-	tableau = malloc(sizeof(int) * height);
-	if (height == 0)
-		return (NULL);
-
+	/* Partie 4 */
 	for (i = 0; i < height; i++)
 	{
-		tableau[i] = malloc(sizeof(int) * width);
+		tableau[i] = (int *)malloc(sizeof(int) * width);
 		if (tableau[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(tableau[j]);
+			}
+			free(tableau);
 			return (NULL);
+		}
 	}
-
 	return (tableau);
 }
 
 /*
 * // Partie 1 //
-on initialise nos variable et double pointeur (un double pointeur est utilisé
-par rapport à celui qui est présent dans notre fonction de base)
+on initialise notre variable et double pointeur (un double pointeur est
+utilisé par rapport à celui qui est présent dans notre fonction de base)
 // Partie 2 //
 Si Width et height sont inférieur ou égal à 0, alors on retour NULL
+Il n'est pas nécessaire de tenter une allocation mémoire pour le tableau, car
+un tableau de dimensions invalides ne peut être créé
 // Partie 3 //
-On alloue de la mémoire à notre hauteur et on vérifie en cas de mauavise
-allocation de mémoire
+si les dimensions sont vérifiés, on alloue de la mémoire à notre hauteur
+et on vérifie en cas de mauvaise allocation de mémoire.
+Cette étape est nécessaire car vous avez un tableau de "height" pointeurs,
+donc on doit allouer cette mémoire avant d'essayer d'allouer la mémoire pour
+chaque ligne idividuelle
+// Partie 4 //
+On initialise ensuite une boucle où on index une valeur où on
+incrémente petit à petit.
+On alloue la mémoire de notre pointeur indexé à i. Pour lui dire qu'on
+alloue de la mémoire en réservant autant d'entier qu'il y a de width sur
+la taile du pointeur (tableau[i]) et on vérifie pour évite une erreur.
 */
