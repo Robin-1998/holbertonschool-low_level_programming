65d79ab2c30be08baf9efee1121e0d7db26a159f
Robin-1998 <10616@holbertonstudents.com>
2025-03-12 15:31:22 +0100

    Projet C, more malloc, last task without commentary

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - fonction qui créé un tableau d'entier
+ * @min : variable dans les paramètres de fonction qui affiche la
+ * premier entier du tableau
+ * @max : variable dans les paramètres de fonction qui affiche le
+ * deuxième entier du tableau
+ * Return: Nothing.
+ */
+
+int *array_range(int min, int max)
+{
+	/* Partie 1 */
+	int *entier;
+	int i, k = 0;
+
+	/* Partie 2 */
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	/* Partie 3 */
+	entier = malloc(sizeof(int) * (min + max + 1));
+	if (entier == NULL)
+	{
+		return(NULL);
+	}
+	else
+	{
+	for (i = min; i <= max; i++)
+		{
+			entier[k] = i;
+			k++;
+		}
+	}
+	return (entier);
+}
+
+/*
+* Partie 1
+On décale notre pointeur qui va stocker la mémoire du tableau d'entier
+Partie 2
+Si min est supérieur à max alors on retourne NULL
+Partie 3
+dans notre pointeur on y stocke un entier de 
+*/
