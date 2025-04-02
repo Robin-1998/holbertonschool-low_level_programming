0534bff39729c78f67f2156e9450743c4e65c5ca
Robin-1998 <10616@holbertonstudents.com>
2025-04-02 15:06:44 +0200

    Correctif betty pour le fichier de la tache 4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,8 @@
 #include "lists.h"
 /**
- * add_dnodeint_end - fonction qui ajoute un nouveau noeud à la fin de
- * la liste chaîné dlistint_t
- * @head: double pointeur du nombre d'entier a ajouter en tête de liste
- * @n: est un pointeur d'entier
- * Return: retourne l'adresse du nouvel élément
+ * free_dlistint - fonction qui libère en mémoire la double liste chaîné
+ * dlistint_t
+ * @head: pointeur du nombre d'entier a ajouter en tête de liste
  */
 
 void free_dlistint(dlistint_t *head)
