3e762b5e087c509bb0c2c215f960fb86956ca346
Robin-1998 <10616@holbertonstudents.com>
2025-04-02 10:09:43 +0200

    ajout du fichier pour la tache 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - fonction qui retourne le nombre d'éléments dans
+ * une liste chaîné dlistint_t
+ * @h: un pointeur constant vers un noeud de la liste doublement châiné
+ * Return :
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nouveaunoeud;
+
+	while (h)
+	{
+		nouveaunoeud++;
+		h = h->next;
+	}
+	return (nouveaunoeud);
+}
