c0f0d05716ece614cb29d342eb624a32c288baee
Robin-1998 <10616@holbertonstudents.com>
2025-04-03 14:33:36 +0200

    Correctif betty task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,13 +3,14 @@
  * insert_dnodeint_at_index - fonction qui insert un nouveau noeud dans
  * une certain position
  * @h: double pointeur qui permet
- * @param idx
- * @param n
- * Return: dlistint_t*
+ * @idx : ??
+ * @n : ??
+ * Return: On retourne l'adresse du nouveau noeud
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *nouveau_noeud = malloc(sizeof(dlistint_t));
+
 	if (nouveau_noeud == NULL)
 	{
 		return (NULL);
@@ -21,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		(*h)->prev = nouveau_noeud;
 	}
-	nouveau_noeud-> n = n;
+	nouveau_noeud->n = n;
 	nouveau_noeud->next = NULL;
 	if (idx == 0)
 	{
